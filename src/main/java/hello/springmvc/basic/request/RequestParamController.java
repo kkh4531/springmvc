141f912726845da0e4f11bc83baa4f3a3d1f0ef2
kimkihyeon <rlgus4531@naver.com>
2025-09-20 23:04:48 +0900

    springmvc http 요청 파라미터-쿼리 파라미터, html form

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1") // get, post 둘 다 되고 그 외의 메소드도 전부 다 실행됨.
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}", username);
+        log.info("age={}", age);
+        response.getWriter().write("ok");
+    }
+}
